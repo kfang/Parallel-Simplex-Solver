b218aa747c151cda97941a95cd41c12ff87a2472
Kevin Fang <kfang@berkeley.edu>
2012-10-31 01:12:52 -0700

    continued on converter

--- MPS_CPPLEX_converter/conv.py
+++ MPS_CPPLEX_converter/conv.py
@@ -2,10 +2,14 @@ from array import array
 
 data = []
 
+
+
+
 file = open('sample.mps', 'r')
 
 line = file.readline()
 
+# for each line of the file, strip out the spaces and store them in data
 while(line):
 	splitLine = line.split(' ')
 
@@ -16,5 +20,18 @@ while(line):
 	data.append(noSpaceLine)
 	line = file.readline()
 
+# last element of data has '\n' in it, get rid of it
 for arr in data:
+	lastElement = arr[len(arr) - 1]
+	arr[len(arr) - 1] = lastElement.split('\n')[0]
 	print arr
+
+# parse out data and store them into objects
+name = ''
+rows = []
+cols = []
+rhs = []
+bounds = []
+
+
+print 'NAME: ' + name
