c0ad02161e04c0e197f4be18aefcfb6940e7b514
Kevin Fang <kfang@lubby.(none)>
2012-11-14 08:23:48 -0800

    did work on Tablaeuaueusa

--- Reader.cpp
+++ Reader.cpp
@@ -36,11 +36,11 @@ int main() {
 
 	string s;
 
-	int numRows = 1;
+	int numRows = 1; //starts with 1 since row 0 is the objective function
 	int numCols = 1; //numCols is offset by 1, col 1 means its actually col 0
-	map<string, int> rowMap;
-	map<string, int> colMap;
-	vector<GridPos> matrixPositions;
+	map<string, int> rowMap; //stores a map from the row name to its row number
+	map<string, int> colMap; //stores a map from the col name to its col number
+	vector<GridPos> matrixPositions; //a list of positions and values for the tableau
 
 	if (file.is_open()) {
 		getline(file, s);
@@ -50,10 +50,13 @@ int main() {
 				//do stuff for ROW
 				getline(file, s);
 				while(s[0] == ' '){
+					//look for the objective function
 					if (s[1] != 'N'){
+						//just a regular row
 						rowMap[s.substr(4, 10)] = numRows;
 						numRows++;
 					} else {
+						//the objective function, place it in row 0
 						rowMap[s.substr(4, 10)] = 0;
 					}
 					getline(file, s);
@@ -61,9 +64,11 @@ int main() {
 			} else if (s.compare("COLUMNS") == 0){
 				//do stuff for COLUMNS
 				getline(file, s);
+				
 				while (s[0] == ' '){{
 					//get the column (variable name)
 					string colName = s.substr(4, 10);
+
 					//strip off whitespace at the end
 					size_t lastChar = colName.find_last_not_of(" ");
 					if (lastChar != string::npos){
@@ -71,13 +76,16 @@ int main() {
 					}
 					
 					//put the column into the map if it doesn't already exist
+					//**you know it doesn't exist if the value of the key is 0
+					//****yes, this is why the numCol is offset by 1 -_-;;
 					if (colMap[colName] == 0){
 						colMap[colName] = numCols;
 						numCols++;
 					}
 
-					//get the row
+					//get the row name
 					string rowName = s.substr(14, 10);
+
 					//strip off the whitespace at the end
 					lastChar = rowName.find_last_not_of(" ");
 					if (lastChar != string::npos){
@@ -94,14 +102,19 @@ int main() {
 
 					//check for another set
 					if (s.length() > 40){
+						//get the row name
 						rowName = s.substr(39, 10);
+
 						//strip off whitespace at the end
 						lastChar = rowName.find_last_not_of(" ");
 						if (lastChar != string::npos){
 							rowName.erase(lastChar+1);
 						}
+						
+						//get the value
 						val = atof (s.substr(50, 15).c_str());
 
+						//generate the tuple, add it to the list
 						GridPos pos2 (rowMap[rowName], colMap[colName] - 1, val);
 						matrixPositions.push_back(pos2);
 					}
@@ -110,35 +123,45 @@ int main() {
 
 				}
 			} else if (s.compare("RHS") == 0){
+				//do stuff for RHS
 				getline(file, s);
 				while (s[0] == ' '){
+					//get the row name
 					string rowName = s.substr(14, 10);
+
 					//strip off whitespace
 					size_t lastChar = rowName.find_last_not_of(" ");
 					if (lastChar != string::npos){
 						rowName.erase(lastChar+1);
 					}
+
 					//store the value
 					double val = atof (s.substr(24, 15).c_str());
 
-					//know what row it goes, have total number of columns
+					//we know what row it goes in, have total number of columns
+					//****remember that numCols is offset by +1
+					//generate the tuple and add it to the list of (position, values)
 					GridPos pos (rowMap[rowName], numCols - 1, val);
 					matrixPositions.push_back(pos);
 
 					//check for another value
 					if (s.length() > 40){
+						//get the row name
 						rowName = s.substr(39, 10);
+
 						//strip whitespace
 						lastChar = rowName.find_last_not_of(" ");
 						if (lastChar != string::npos){
 							rowName.erase(lastChar+1);
 						}
+
 						//get the value
 						val = atof (s.substr(50, 15).c_str());
+						
+						//generate the tuple and add it to the list
 						GridPos pos2 (rowMap[rowName], numCols -1, val);
 						matrixPositions.push_back(pos2);
 					}
-
 					getline(file, s);
 				}
 			} else {
@@ -146,6 +169,8 @@ int main() {
 			}
 		}
 	} else {
+		//if no file gets open, return -> we don't want to simplex on 
+		//empty stuff... bad will happen
 		return 1;
 	}
 	file.close();
@@ -158,7 +183,12 @@ int main() {
 	//Create the tableau object and populate it from the list of values created while
 	//parsing the MPS file
 	Tableau tableau (numRows, numCols);
+
+	//inteate down the list (actually a vector) to populate the tableau with values
 	for (int i = 0; i < matrixPositions.size(); i++){
+		//this line looks scary, but its simply
+		//tableau[row][col] = val;
+		//where row and col come from the matrixPositions list
 		tableau[matrixPositions[i].row][matrixPositions[i].col] = matrixPositions[i].val;
 	}
 
