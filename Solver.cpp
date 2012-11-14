de428145003e167e715ecf25877b186d2f8ab58f
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-14 01:24:25 -0800

    Started setting up the solver

---
+++ Solver.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <stdlib.h>
+#include <vector>
+#include "GridPos.cpp"
+#include "Tableu.cpp"
+
+using namespace std;
+
+class Solver {
+	Tableau tableau;
+public:
+	Solver (Tableau);
+	void solve();
+	list<int> possible_incoming();
+	int select_incoming(list<int>);
+	int select_outgoing();
+	bool unbounded_check();
+	void pivot(int, int);
+	bool check_if_done();
+};
+
+void Solver::solve() {
+	while(!check_if_done()) {
+		int incoming = select_incoming(possible_incoming());
+		int outgoing = select_outgoing();
+		if (unbounded_check()) {
+			break;
+		}
+		pivot(incoming, outgoing);
+	}
+}
