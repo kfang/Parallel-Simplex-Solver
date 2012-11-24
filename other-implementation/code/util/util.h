7e9aed7e3fbe2133f9eb1b3e40814bcda15de1be
Martin_Ortega <cs194-al@hive21.CS.Berkeley.EDU>
2012-11-24 15:49:26 -0800

    added util.h

---
+++ other-implementation/code/util/util.h
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////
+//                                 util.h                                 //
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+
+//--------------------------------------------------------------------------
+// CREATE_2D_ARRAY
+
+template<class T>
+T** create_2D_array(int num_rows, int num_cols)
+{
+	T** array = new T*[num_rows]();
+	assert(array != 0);
+	for (int row = 0; row < num_rows; row++) {
+		array[row] = new T[num_cols]();
+		assert(array[row] != 0);
+	}
+
+	return array;
+}
+
+//--------------------------------------------------------------------------
+// PRINT_MATRIX
+
+void print_matrix(int num_rows, int num_cols, float** M)
+{
+	for (int row = 0; row < num_rows; row++) {
+		for (int col = 0; col < num_cols; col++) {
+			printf("%8.2f", M[row][col]);
+		}
+		std::cout << std::endl;
+	}
+}
+
+//--------------------------------------------------------------------------
+// TIMESTAMP
+
+#endif
