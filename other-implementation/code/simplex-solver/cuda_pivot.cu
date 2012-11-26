5de231ef8c19bd162a7a888568e18e6a3c68769c
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-26 00:31:13 -0800

    check value on gpu

--- other-implementation/code/simplex-solver/cuda_pivot.cu
+++ other-implementation/code/simplex-solver/cuda_pivot.cu
@@ -20,5 +20,9 @@ __global__ void cuda_pivot(int pivot_row, int pivot_col,
 }
 
 __global__ void cuda_test(int* vals) {
-	*vals = 2;
+	if (*vals != 1) {
+		int k = 4/0;
+	} else {
+		*vals = 2;
+	}
 }
