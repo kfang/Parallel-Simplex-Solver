7fd3f8848f630f721e4b884e072a1fddbb8bf511
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-25 16:12:25 -0800

    Added a python script to create mps problems

---
+++ mps_files/lp_creator.py
@@ -0,0 +1,45 @@
+import sys
+from random import randint
+
+def main():
+	num_rows = int(sys.argv[2])
+	num_cols = int(sys.argv[3])
+	name = sys.argv[1]
+	print "NAME          " + name
+	print "ROWS"
+	print " N  profit"
+	for i in range(num_rows):
+		print " L  r" + str(i)
+	print "COLUMNS"
+	for i in range(num_cols):
+		obj = "    c" + str(i)
+		obj = spaces(obj, 14)
+		obj = obj + "profit"
+		obj = spaces(obj, 35)
+		obj = obj + str(randint(1,9))
+		print obj
+		sys.stderr.write(str(i) + '\n')
+		for j in range(num_rows):
+			l = "    c" + str(i)
+			l = spaces(l, 14)
+			l = l + "r" + str(j)
+			l = spaces(l, 35)
+			l = l + str(randint(1,9))
+			print l
+	print "RHS"
+	for i in range(num_rows):
+		l = "    rhs"
+		l = spaces(l, 14)
+		l = l + "r" + str(i)
+		l = spaces(l, 35)
+		l = l + str(randint(1,9))
+		print l
+	print "ENDATA"
+
+def spaces(l, i):
+	while(len(l) < i):
+		l = l + " "
+	return l
+
+if __name__ == '__main__':
+    main()
