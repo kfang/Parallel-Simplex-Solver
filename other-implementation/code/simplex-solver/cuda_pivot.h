1343fec9e0c51dc53195aa9f53a59f79291b62a2
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-25 23:16:26 -0800

    tests

--- other-implementation/code/simplex-solver/cuda_pivot.h
+++ other-implementation/code/simplex-solver/cuda_pivot.h
@@ -8,6 +8,8 @@
 #ifndef CUDA_PIVOT_H_
 #define CUDA_PIVOT_H_
 
+__global__ void helloCUDA(float f);
+
 __global__ void cuda_pivot(int pivot_row, int pivot_col,
 		int num_rows, int num_cols,
 		float** tableau);
