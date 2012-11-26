055ed2d58e70369f5d92ce9a76b0c870fd6beab5
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-26 03:42:16 -0800

    fix concurency bug

--- other-implementation/code/simplex-solver/cuda_pivot.cu
+++ other-implementation/code/simplex-solver/cuda_pivot.cu
@@ -12,9 +12,13 @@ __global__ void cuda_pivot(int pivot_row, int pivot_col,
 	int col = threadIdx.x;
 
 	// Calculate new value in tableau
-	if (row != pivot_row) {
+	if (row != pivot_row && col != pivot_col) {
 		float scale = tableau[row*num_cols + pivot_col]/pivot_val;
 		tableau[row*num_cols + col] -= scale*tableau[pivot_row*num_cols + col];
+	}
+	syncThreads();
+	if (row != pivot_row && col == pivot_col) {
+		tableau[row*num_cols + col] = 0.0;
 	}
 
 }
