3fda1513d8f36bfc11c8600d9087bfab7b0f080f
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-29 21:46:12 -0800

    Forgot to limit amount

--- other-implementation/code/simplex-solver/cuda_pivot.cu
+++ other-implementation/code/simplex-solver/cuda_pivot.cu
@@ -12,10 +12,11 @@ __global__ void cuda_pivot(int pivot_row, int pivot_col,
 	int col = blockIdx.y * blockDim.y + threadIdx.y;
 
 	// Calculate new value in tableau
-	if (row != pivot_row && col != pivot_col) {
-		float scale = tableau[row*num_cols + pivot_col]/pivot_val;
-		tableau[row*num_cols + col] -= scale*tableau[pivot_row*num_cols + col];
-		tableau[row*num_cols + col] = 30;
+	if (row <= num_rows && col <= num_rows) {
+		if (row != pivot_row && col != pivot_col) {
+			float scale = tableau[row*num_cols + pivot_col]/pivot_val;
+			tableau[row*num_cols + col] -= scale*tableau[pivot_row*num_cols + col];
+		}
 	}
 
 }
