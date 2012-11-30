cfe6b9857eede4ad6f09bc982ba2370b3e5561fc
Alex Kaiser <atkaiser@berkeley.edu>
2012-11-29 19:31:18 -0800

    cuda prints

--- other-implementation/code/simplex-solver/cuda_simplex_solver.cu
+++ other-implementation/code/simplex-solver/cuda_simplex_solver.cu
@@ -150,7 +150,7 @@ void Cuda_Simplex_Solver::pivot(const int& pivot_row, const int& pivot_col,
 	dim3 threads(16,16);
 	int num_blocks = std::max(num_rows, num_cols);
 	num_blocks = ceil((sqrt(num_blocks)+1)/256);
-	std::cerr << "Num of blocks: " num_blocks << std::endl;
+	std::cerr << "Num of blocks: " << num_blocks << std::endl;
 	dim3 blocks(num_blocks, num_blocks);
 
 	cuda_pivot <<< blocks, threads >>> (pivot_row, pivot_col, num_rows, num_cols, cuda_tableau);
